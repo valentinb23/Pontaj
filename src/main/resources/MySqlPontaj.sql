68295be3051bf6f540d39925b6c9044a8a047c5e
vali <vali@vali-PC>
2017-12-15 14:16:02 +0200

    tabel my sql

---
+++ src/main/resources/MySqlPontaj.sql
@@ -0,0 +1,6 @@
+CREATE TABLE pontaj.pontaj ( 
+zi VARCHAR(2) NOT NULL , 
+nume VARCHAR(20) NOT NULL , 
+start VARCHAR(5) NOT NULL , 
+stop VARCHAR(5) NOT NULL , 
+diferenta INT NOT NULL , PRIMARY KEY (zi)); 
